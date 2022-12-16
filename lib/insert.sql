9f928c11e7147ed638f03feba86faf7262b805c7
Hoang Tran <jashoang2583@gmail.com>
2022-12-15 17:51:04 -0800

    commit tree d0523f5a351e5eed9bce689555eb3f44ea750991

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Mr.Chocolate','male','1', 'chocolate', 'angry af', '1');
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Rowdy','male','1', 'chocolate', 'angry af', '1');
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Tabitha','male','1', 'chocolate', 'angry af', '1');
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Sergeant Brown','female','1', 'chocolate', 'angry af', '1');
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Melissa','female', 1, 'chocolate', 'angry af', 1);
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Grinch','male','1', 'chocolate', 'angry af', '1');
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values ('Wendy','female','1', 'chocolate', 'angry af', '1');
+INSERT INTO bears (name, sex, age, color, temperament, alive) Values (null, null, null, null, null, null);
